4cbe46ef89ccccc8f984f647c4ffdf51d964c0d2
Ilia Lentochnik <ilia_lentochnik@epam.com>
2022-04-22 13:46:27 +0000

    Fixes after merge

--- saturn-operator/src/main/java/com/epam/saturn/operator/service/validation/ValidationStrings.java
+++ saturn-operator/src/main/java/com/epam/saturn/operator/service/validation/ValidationStrings.java
@@ -4,6 +4,6 @@ public class ValidationStrings {
 
     public static final String cardNumberRegexp= "[0-9]{16}";
     public static final String phoneNumberRegexp= "^(\\+7|7|8|9)?[\\s\\-]?\\(?[49][0-9]{2}\\)?[\\s\\-]?[0-9]{3}[\\s\\-]?[0-9]{2}[\\s\\-]?[0-9]{2}$";
-    public static final String accountNumberRegexp= "[0-9]{1,10}";
+    public static final String accountNumberRegexp= "^\\d{20}$";
 
 }
