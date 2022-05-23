0721736012fab5cbab3a19228c292a9a746fad8f
puppymail <acc1307121020@gmail.com>
2022-05-23 16:48:25 +0300

    Temporarily disable authorization.

--- saturn-operator/src/main/java/com/saturn_bank/operator/configuration/SecurityConfig.java
+++ saturn-operator/src/main/java/com/saturn_bank/operator/configuration/SecurityConfig.java
@@ -53,9 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/users", "/users/")
-                .hasRole("STAFF")
-                .antMatchers("/", "/**")
+                .anyRequest()
                 .permitAll()
                 .and()
                 .formLogin()
